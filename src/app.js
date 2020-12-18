889206f4799c87ecb829578f0b8f9e0e9d63943a
heejungjung <junghee528@naver.com>
2020-12-18 20:38:08 +0900

    First_commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+let fs = require('fs');
+const options = {
+	key: fs.readFileSync('./keys/private.pem'),
+	cert: fs.readFileSync('./keys/public.pem')
+};
+
 let express = require( 'express' );
 let app = express();
-let server = require( 'http' ).Server( app );
+let server = require( 'https' ).Server( options, app );
 let io = require( 'socket.io' )( server );
 let stream = require( './ws/stream' );
 let path = require( 'path' );
@@ -12,7 +18,6 @@ app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 app.get( '/', ( req, res ) => {
     res.sendFile( __dirname + '/index.html' );
 } );
-
 
 io.of( '/stream' ).on( 'connection', stream );
 
